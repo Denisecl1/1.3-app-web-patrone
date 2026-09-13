ce20e2929e7a4b69cd584745a17be94aeb09e177
Denisecl1 <denisecamp21@gmail.com>
2026-09-13 17:03:21 -0600

    Conectar analizador visual con Vercel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 const API_URL =
 
-    "https://TU-PROYECTO.vercel.app/api/analyze";
+    "https://https://1-3-app-web-patrone-hcvner8ov-denisecl1.vercel.app/api/analyze";
 
 
 const form = document.getElementById("analyzeForm");
